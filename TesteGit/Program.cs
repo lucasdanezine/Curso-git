dcca7fce5a4a338c5bb3d5c3f6ad0245bbd63275
Lucas Danezine <ldanezine@gmail.com>
2020-05-29 01:09:36 -0300

    Mudança de teste

--- TesteGit/Program.cs
+++ TesteGit/Program.cs
@@ -10,7 +10,7 @@ namespace TesteGit
             Console.WriteLine("bom dia!");
             Console.WriteLine("Como você esta hoje?");
             Console.WriteLine("Tenha um bom dia!");
-            Console.WriteLine("Tchau!");
+            Console.WriteLine("tchau nada");
             Console.ReadLine();
         }
     }
